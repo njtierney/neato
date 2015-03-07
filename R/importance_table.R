621ffd105684867d4e99febf3d4a9bcd723b9800
tierneyn <nicholas.tierney@gmail.com>
2015-03-07 15:26:59 +1000

    First Push
    First Push, haven’t tested anything yet. Currently just working on the
    importance_table function. Hopefully this exercise proves to be a
    useful experience in writing R packages, so that I can write them
    faster in the future.

---
+++ R/importance_table.R
@@ -0,0 +1,83 @@
+#' importance_table
+#'
+#' \code{importance_table} returns a data_frame of variable importance
+#'
+#' This currently only works for rpart and gbm.step functions. In the
+#' future more features will be added so that it works for many
+#' decision trees
+#'
+#' @param x An rpart of gbm.step object to be turned into a tidy dataframe
+#'     containing importance values
+#'
+#' @return A data_frame object made with the intention of turning it into a
+#'     text table with `knitr` or `xtable`
+#'
+#' @examples
+#'
+#' # turn a fitted object into a data_frame
+#'
+#'  \code{rpart} object
+#' fit.rpart <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+#' importance_table(fit.rpart)
+#'
+#' \code{gbm.step} object
+#' fit.gbm.step <- gbm.step(data = iris,
+#'                          gbm.x = c(1:3),
+#'                          gbm.y = 4,
+#'                          tree.complexity = 1,
+#'                          family = "gaussian",
+#'                          learning.rate = 0.01,
+#'                          bag.fraction = 0.5)
+#'
+#' importance_table(fit.gbm.step)
+#'
+#' Unfortunately it cannot yet run a \code{gbm} object:
+#' \dontrun{
+#'
+#' gbm.fit <- gbm(Sepal.Width ~ .,
+#'                distribution = "gaussian",
+#'                data = iris)
+#'
+#' importance_table(gbm.fit)
+#' }
+#'
+#' @seealso \url{https://github.com/dgrtwo/broom}
+#'
+#'
+#'
+#'
+importance_table <- function(x){ # rpart or gbm object
+
+#========
+# rpart
+#========
+  if (class(x) == rpart){
+    # make a kable plot for the variable importance from the CART model
+
+    x$variable.importance %>%
+      data.frame(variable = names(x$variable.importance),
+                 importance = as.vector(x$variable.importance),
+                 row.names = NULL) %>%
+      select(variable,
+             importance)
+
+#=====
+# gbm
+#=====
+
+  } else if (class(x) == gbm) {
+
+      x$contributions %>%
+        # make it a dataframe
+        as_data_frame %>%
+        # rename the variables
+        rename(variable = var,
+               relative.imp = rel.inf)
+
+#=============
+# error catch
+#=============
+
+    } else stop("x is not an rpart or gbm.step type object")
+
+} # end function
