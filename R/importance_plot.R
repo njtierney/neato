43f149155cc0164921b9e263888cbc2c3755df69
tierneyn <nicholas.tierney@gmail.com>
2015-03-09 14:35:01 +1000

    renamed vimp_plot to importance_plot
    I decided to change the name of brt_vimp_plot to something that was
    more specific and unabbreviated. importance_plot tells me that it is a
    plot of the importance values. This function also uses the code from
    importance_table to run.

---
+++ R/importance_plot.R
@@ -0,0 +1,68 @@
+#' importance_plot
+#'
+#' \code{importance_plot} make a graph of variable importance
+#'
+#' takes an `rpart` or `gbm.step` fitted object and makes a plot of variable
+#' importance
+#'
+#' @param x is an rpart or gbm.step object
+#'
+#' @return a ggplot plot of the variable importance
+#'
+#' @examples
+#' turn a fitted object into a data_frame
+#'
+#'  \code{rpart} object
+#' fit.rpart <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+#'
+#' importance_plot(fit.rpart)
+#'
+#' # you can even use piping
+#'
+#' fit.rpart %>% importance_plot
+#'
+#' Required packages
+#'
+#' dplyr
+#' reshape2
+#' mice
+#' ggplot2
+#'
+#' @export
+importance_plot <- function(x){
+
+  # this is a plot to make it easier to get the interactions out into a table
+
+  # this function takes an importance_table object and then
+
+  if (class(x) == "imp_tbl"){
+
+    x %>%
+      # make sure the plot is ordered by most important
+      transform(.,
+                variable=reorder(variable,
+                                 importance)) %>%
+      # plot them!
+      ggplot(data = .,
+             aes(x = variable,
+                 y = importance)) +
+      geom_point()
+
+  } else if (class(x) != "imp_tbl"){
+
+  x %>%
+  importance_table %>%
+    # make sure the plot is ordered by most important
+    transform(.,
+              variable=reorder(variable,
+                               importance)) %>%
+    # plot them!
+    ggplot(data = .,
+           aes(x = variable,
+               y = importance)) +
+    geom_point()
+
+  } # end ifelse
+
+} # end function
+
