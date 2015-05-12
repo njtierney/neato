35d97f0109bb0317a52ff91cbdc047be02f70f5b
tierneyn <nicholas.tierney@gmail.com>
2015-05-12 12:51:26 +1000

    more general code + bug fix
    fixed a bug with the ! %in%, and also added a random forest method.
    Soon…Soon it will be converted to S3

--- R/importance_plot.R
+++ R/importance_plot.R
@@ -21,6 +21,23 @@
 #'
 #' fit.rpart %>% importance_plot
 #'
+#'  \code{randomForest} object
+#'
+#'  set.seed(131)
+#'   ozone.rf <- randomForest(Ozone ~ ., data=airquality, mtry=3,
+#'                            importance=TRUE, na.action=na.omit)
+#'   print(ozone.rf)
+#'   ## Show "importance" of variables: higher value mean more important:
+#'   importance(ozone.rf)
+#'
+#'   ## use importance_table
+#'
+#'   importance_table(ozone.rf)
+#'
+#'   # now plot it
+#'
+#'   importance_plot(ozone.rf)
+#'
 #' @import dplyr
 #'
 #' @export
@@ -30,20 +47,36 @@ importance_plot <- function(x){
 
   # this function takes an importance_table object and then
 
-  if (class(x) == "imp_tbl"){
+  if ("imp_tbl" %in% class(x)){
 
     x %>%
       # make sure the plot is ordered by most important
       transform(.,
-                variable=reorder(variable,
-                                 importance)) %>%
+                variable = reorder(variable,
+                                   importance)) %>%
       # plot them!
       ggplot2::ggplot(data = .,
              ggplot2::aes(x = importance,
                           y = variable)) +
       ggplot2::geom_point()
 
-  } else if (class(x) != "imp_tbl"){
+  } else if ("randomForest" %in% class(x)) {
+
+    importance_table(x) %>%
+      tidyr::gather(variable,
+             value) %>%
+      transform(.,
+                variable = reorder(variable,
+                                   value)) %>%
+      ggplot2::ggplot(data = .,
+                      ggplot2::aes(x = value,
+                                   y = variable)) +
+      ggplot2::geom_point() +
+      ggplot2::facet_wrap(~ variable.1,
+                          scales = "free") +
+      ggplot2::theme(axis.text.x = element_text(angle = 45))
+
+  } else if (!("imp_tbl" %in% class(x))) {
 
   x %>%
   importance_table %>%
