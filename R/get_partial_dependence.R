bf51ec4c1caa610494750e062793e1b2aca2d451
Nicholas Tierney <nicholas.tierney@gmail.com>
2015-10-08 14:27:50 +1000

    Temporarily disable these function
    I know this isn’t the best way to make functions not work, but it’s a
    work around until I get them working the way I want to.

--- R/get_partial_dependence.R
+++ R/get_partial_dependence.R
@@ -1,32 +1,30 @@
-#' partial_dependence
-#' 
-#' 
-#' 
-#' 
-#' Description: A method that returns the partial dependence values for a given set of variables for a gbm.step model. Would be really great in the future to make this a  
-
-
-
-get_partial_dependence <- function(x, var){
-  
-  i <- which(x$var.names == var)
-  
-  response_matrix <- plot.gbm(x,
-                              i.var = i,
-                              n.trees = x$n.trees,
-                              return.grid = TRUE)
-  
-  # Mean Centre the data...
-  #     df_box[[i]] <- data.frame(value = response_matrix[ ,1],
-  #                               fitted_function = response_matrix[ ,2] -
-  #                                 mean(response_matrix[ ,2])) %>%
-  #       mutate(variable = x$var.names[i])
-  
-  # not mean centered
-  
-  df <- data.frame(value = response_matrix[ , 1],
-                   fitted_function = response_matrix[ , 2]) %>%
-    mutate(variable = x$var.names[i])
-  
-  return(df)
-}
+#' #' partial_dependence
+#' #'
+#' #'
+#' #'
+#' #'
+#' #' Description: A method that returns the partial dependence values for a given set of variables for a gbm.step model. Would be really great in the future to make this a
+#'
+#' get_partial_dependence <- function(x, var){
+#'
+#'   i <- which(x$var.names == var)
+#'
+#'   response_matrix <- plot.gbm(x,
+#'                               i.var = i,
+#'                               n.trees = x$n.trees,
+#'                               return.grid = TRUE)
+#'
+#'   # Mean Centre the data...
+#'   #     df_box[[i]] <- data.frame(value = response_matrix[ ,1],
+#'   #                               fitted_function = response_matrix[ ,2] -
+#'   #                                 mean(response_matrix[ ,2])) %>%
+#'   #       mutate(variable = x$var.names[i])
+#'
+#'   # not mean centered
+#'
+#'   df <- data.frame(value = response_matrix[ , 1],
+#'                    fitted_function = response_matrix[ , 2]) %>%
+#'     mutate(variable = x$var.names[i])
+#'
+#'   return(df)
+#' }
--- R/gg_partial_plot.R
+++ R/gg_partial_plot.R
@@ -1,47 +1,49 @@
-#' gg_partial_plot
-#' 
-#' 
-#' 
-#' # this uses the "get_partial_dependence" function to then plot the partial dependence.
-#' Would be really great to generalize this to RPART, BRT, and RF models
-
-# note, would be good to set this up as an S3 method...
-
-# what are the variables that we want to plot?
-
-important_vars <- c("LF", "sys", "dias", "crs", "bmi")
-
-df_box <- list("vector", length(important_vars))
-
-for (i in (1:length(important_vars))){
-  
-  df_box[[i]] <- get_partial_dependence(brt.ghs, 
-                                        important_vars[[i]])
-  
-}
-
-df <- bind_rows(df_box)
-
-# make another 
-
-df.mean <- 
-  df %>% 
-  group_by(variable) %>%
-  summarise(mean = mean(fitted_function))
-
-ggplot(data = df,
-       aes(x = value,
-           y = fitted_function)) + 
-  geom_line() + 
-  facet_wrap(~variable,
-             ncol = 2,
-             scales = "free_x") +
-  geom_hline(data = df.mean,
-             aes(yintercept = mean),
-             colour = "red",
-             linetype = "dashed",
-             alpha = 0.75) +
-  geom_hline(aes(yintercept = 3.729),
-             colour = "orange") + 
-  geom_hline(aes(yintercept = 12.675),
-             colour = "green")
+#' #' gg_partial_plot
+#' #'
+#' #'
+#' #'
+#' #' # this uses the "get_partial_dependence" function to then plot the partial dependence.
+#' #' Would be really great to generalize this to RPART, BRT, and RF models
+#'
+#' # note, would be good to set this up as an S3 method...
+#'
+#' # what are the variables that we want to plot?
+#'
+#'
+#'
+#' important_vars <- c("LF", "sys", "dias", "crs", "bmi")
+#'
+#' df_box <- list("vector", length(important_vars))
+#'
+#' for (i in (1:length(important_vars))){
+#'
+#'   df_box[[i]] <- get_partial_dependence(brt.ghs,
+#'                                         important_vars[[i]])
+#'
+#' }
+#'
+#' df <- bind_rows(df_box)
+#'
+#' # make another
+#'
+#' df.mean <-
+#'   df %>%
+#'   group_by(variable) %>%
+#'   summarise(mean = mean(fitted_function))
+#'
+#' ggplot(data = df,
+#'        aes(x = value,
+#'            y = fitted_function)) +
+#'   geom_line() +
+#'   facet_wrap(~variable,
+#'              ncol = 2,
+#'              scales = "free_x") +
+#'   geom_hline(data = df.mean,
+#'              aes(yintercept = mean),
+#'              colour = "red",
+#'              linetype = "dashed",
+#'              alpha = 0.75) +
+#'   geom_hline(aes(yintercept = 3.729),
+#'              colour = "orange") +
+#'   geom_hline(aes(yintercept = 12.675),
+#'              colour = "green")
