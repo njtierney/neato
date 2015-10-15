207390c65f8271b57aac9977e032d1ba9e08ae5f
Nicholas Tierney <nicholas.tierney@gmail.com>
2015-10-15 15:49:19 +1000

    new RSS
    updated changes for RSS

--- R/rss.R
+++ R/rss.R
@@ -1,51 +1,58 @@
-#' RSS
+#' rss
+#'
 #' \description A function that gives returns the RSS of a decision tree model.
 #'
+#' @param x A fitted decision tree model - either `rpart`, `gbm` or `randomForest`.
 #'
-#' Constructor functions
+#' @return The Residuals Sums of Squares (RSS) $\sum_i^n(Y_i - \hat(Y))^2$ for the models `rpart`, `gbm.step`, and `randomForest`.
+#'
+#' @note when using the `caret` package, be sure to select `model$finalModel` when entering it into the `rss` function. Also note that the RSS only works for continuous variables.
+#'
+#' @examples
+#'
+#' library(rpart)
+#'
+#' fit.rpart <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
+#'
+#' rss(fit.rpart)
+#'
+#' @export
+
+#=======================#
+# Constructor functions #
+#=======================#
 
 rss <- function(x){
-  
-  UseMethod(rss)
-  
+
+  UseMethod("rss", x)
+
 }
 
-#' 
-#' 
-#' 
-#' 
-#' CART
+#=====================================#
+#' Classification and Regression Tree #
+#=====================================#
 
 rss.rpart <- function(x){
-  
+
   sum((residuals(x)^2))
-  # cart_ghs_rss <- sum((residuals(rpart.ghs)^2))  
+
 }
 
-
-#' BRT
-
+#==========================#
+#' Boosted Regression Tree #
+#==========================#
 rss.gbm <- function(x){
-  
+
   sum(x$residuals^2)
-  # brt_ghs_rss <- sum(brt.ghs$residuals^2)  
+
 }
-
-
-#' Random Forest
-#' 
-
+#================#
+#' Random Forest #
+#================#
 rss.randomForest <- function(x){
 
-  res <- x$finalModel$y - x$finalModel$predicted  
-  
-  sum(res^2)  
-    
-#   residual_rf <- rf_ghs_caret$finalModel$y - rf_ghs_caret$finalModel$predicted  
-#   rf_ghs_rss <- sum(residual_rf^2)  
-}
+  res <- x$y - x$predicted
 
+  sum(res^2)
 
-
-
-
+} # end function
