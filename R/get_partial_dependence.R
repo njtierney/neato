cf27035484ce3ad3d9df92b20ccf274e59141df3
Nicholas Tierney <nicholas.tierney@gmail.com>
2015-11-10 15:49:02 +1000

    export `get_partial_dependence` function

--- R/get_partial_dependence.R
+++ R/get_partial_dependence.R
@@ -3,7 +3,7 @@
 #' @description : Some code that returns the partial dependence values for a given set of variables for a gbm.step model. In the future this function will work.
 #'
 #' @note This requires the loading of the `gbm.step` function. Hopefully sometime soom I can just write this in vanilla R myself. Future extensions will allow for this function to work for `rpart`, `gbm`, `gbm.step`, and `randomForest`.
-
+#' @export
 get_partial_dependence <- function(x, var){
 
   # grab the name sof the variables in the dataframe used in the model, and give their vector columns position to `i`
