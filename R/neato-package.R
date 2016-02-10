1070b0a29a0345e0bd95474b2937525a50201320
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-02-10 14:46:27 +1000

    created new central spot for imports

---
+++ R/neato-package.R
@@ -0,0 +1,13 @@
+#' neato
+#'
+#' @name neato
+#' @docType package
+#' @importFrom gridExtra grid.arrange
+#' @importFrom tidyr gather
+#' @import dplyr
+#' @import ggplot2
+#' @import reshape2
+#' @import gbm
+NULL
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
